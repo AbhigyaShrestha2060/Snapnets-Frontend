eda49b9b9262f3b0197c018e5fda98b574790c56
AbhigyaShrestha2060 <abhigyashrestha730@gmail.com>
2025-05-30 22:25:09 +0545

    changed in the api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'https://snapnets-backend.onrender.com/' + '/api',
+  baseURL: 'https://snapnets-backend.onrender.com' + '/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'multipart/form-data',
